a9d1415a47831f9e080aaf59ce24b81ab43d9e9c
Argeu Aprigio Alcantara <argeualcantara@users.noreply.github.com>
2023-08-24 13:02:44 -0300

    Fix tweet sidebar button

--- old-twitter.js
+++ old-twitter.js
@@ -1,10 +1,3 @@
-// Setting language, try getting value from cookie, then browser
-const cookieLang = document.cookie
-.split("; ")
-.find((row) => row.startsWith("lang="))
-.split("=")[1];
-const lang = cookieLang? cookieLang.toLowerCase(): navigator.language.toLowerCase();
-
 // Twitter.com code
 if (location.href.indexOf("tweetdeck") == -1) {
     var i = 0;
@@ -31,6 +24,13 @@ if (location.href.indexOf("tweetdeck") == -1) {
     }, 10);
     
     setInterval(() => {
+        // Setting language, try getting value from cookie, then browser
+        var cookieLang = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("lang="))
+        .split("=")[1];
+        var lang = cookieLang? cookieLang.toLowerCase(): navigator.language.toLowerCase();
+        
         var logo = document.querySelector("h1 div > svg");
         if (logo) {
             logo.setAttribute("viewBox", "0 0 16 16");
@@ -40,9 +40,9 @@ if (location.href.indexOf("tweetdeck") == -1) {
                 logoPath.setAttribute("d", "M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z");
             }
         }
-        var bttn = document.querySelector("#react-root > div > div > div > header > div > div > div > div > div > a > div > span > div > div > span > span");
-        if (bttn) {
-            bttn.innerText = (lang == "pt-BR"?"Tweetar":"Tweet");
+        var sideBarBtn = document.querySelector("#react-root > div > div > div > header > div > div > div > div > div > a > div > span > div > div > span > span");
+        if (sideBarBtn) {
+            sideBarBtn.innerText = (lang.indexOf("pt") != -1?"Tweetar":"Tweet");
         }
         var title = document.querySelector("head > title").innerHTML
         if (title) {
